536741c5c0bf8dc07fb456cc333b0bfb1b5b8167
Merkury32 <pjozefiak32@gmail.com>
2020-07-24 13:36:02 +0200

    change global arrays to local

--- src/app/services/userService/user.service.ts
+++ src/app/services/userService/user.service.ts
@@ -6,36 +6,38 @@ import { User } from 'src/app/models/user.model';
   providedIn: 'root',
 })
 export class UserService {
-  usersArr = [];
 
   constructor() {}
 
   fetchUsers() {
     let newUser = JSON.parse(sessionStorage.getItem('users'));
 
-    console.log('userService fetch users:')
+    console.log('userService fetch users:');
     console.log(newUser);
 
     return of(newUser);
   }
 
   addUser(user: User) {
+
+    let usersArr = JSON.parse(sessionStorage.getItem('users')) || [];
+
     user.id = Math.random();
 
-    console.log('userService add user:')
+    console.log('userService add user:');
     console.log(user);
 
-    this.usersArr.push(user);
-    sessionStorage.setItem('users', JSON.stringify(this.usersArr));
+    usersArr.push(user);
+    sessionStorage.setItem('users', JSON.stringify(usersArr));
     return of(this.fetchUsers());
   }
 
   deleteUser(id) {
     console.log(`userService deleteUser with id: ${id}`);
 
-    this.usersArr = JSON.parse(sessionStorage.getItem('users'));
-    this.usersArr.splice(id, 1);
-    sessionStorage.setItem('users', JSON.stringify(this.usersArr));
-    return of(this.usersArr);
+    let deletedUser = JSON.parse(sessionStorage.getItem('users')) || [];
+    deletedUser.splice(id, 1);
+    sessionStorage.setItem('users', JSON.stringify(deletedUser));
+    return of(deletedUser);
   }
 }
