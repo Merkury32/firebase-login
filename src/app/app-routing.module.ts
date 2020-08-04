5cd4b5bd90d110c9e8e87519eb0c3c46df683477
Merkury32 <pjozefiak32@gmail.com>
2020-08-04 15:10:55 +0200

    add: hash routing
    (cherry picked from commit 99d115191da3c5a3d6809dfc80234a7dbe99ff43)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UsersEditComponent } from './pages/users-edit/users-edit.component';
 
-
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'edit', component: UsersEditComponent }
+  { path: 'edit', component: UsersEditComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
