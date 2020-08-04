257a466ada8dec4258e1ca0a586c258454fb934a
Merkury32 <pjozefiak32@gmail.com>
2020-08-04 15:07:47 +0200

    add: auth email and password check
    (cherry picked from commit ae67b0fae9914c0ecef7f399f7d682dacaeb7dcb)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms'
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/authService/auth.service'
+import { AuthService } from '../../services/authService/auth.service';
 import { User } from 'src/app/models/user.model';
 import { Observable } from 'rxjs';
 import { UserLogin } from 'src/app/models/user-login.model';
@@ -9,15 +9,15 @@ import { UserLogin } from 'src/app/models/user-login.model';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   user: User[];
+  isWrongData = false;
 
-  constructor(private router: Router, private authService: AuthService) { }
+  constructor(private router: Router, private authService: AuthService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   onSubmit(form: NgForm) {
     const email = form.value.email;
     const password = form.value.password;
@@ -31,9 +31,11 @@ export class LoginComponent implements OnInit {
     authObs.subscribe(
       (resData) => {
         console.log(resData);
+        this.isWrongData = false;
         this.router.navigate(['/edit']);
       },
       (errorMessage) => {
+        this.isWrongData = true;
         console.log(errorMessage);
       }
     );
