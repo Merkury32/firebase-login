627952ad94ae878c833bdd62c1cf9864c0f04cd6
Merkury32 <pjozefiak32@gmail.com>
2020-08-19 12:59:13 +0200

    change function type
    (cherry picked from commit f949e3f5d274ad928fe266988dde4178a4060e9e)

--- src/app/services/authService/auth.service.ts
+++ src/app/services/authService/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
         this.router.navigate(['edit']);
         console.log('Succes');
       })
-      .catch(function (error) {
+      .catch((error) => {
         console.log(error);
       });
   }
